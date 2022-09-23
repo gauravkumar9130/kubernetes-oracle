bf52d6260aed7f9af8d4ef2f2b56b95aaea0de55
root <root@master.publicsubnet.demovcn.oraclevcn.com>
2022-09-23 01:57:15 +0000

    oracle kubernetes setup

---
+++ worker1.sh
@@ -0,0 +1,54 @@
+echo "********************************* Prerequisite **************************************"
+yum install vim wget -y
+con=`nmcli connection show | grep ethernet | awk '{print $1}'`
+nmcli connection modify $con connection.autoconnect yes
+hostnamectl set-hostname worker1
+vim -c "g/swap/d" -c "wq" /etc/fstab
+vim -c "7s/enforcing/permissive/g" -c "wq" /etc/sysconfig/selinux
+swapoff -a
+setenforce 0
+systemctl stop firewalld
+systemctl disable firewalld
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+
+echo "********************************* Configure Kubernetes **************************************"
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum clean all -y
+yum repolist all -y
+yum install kubectl kubelet kubeadm -y
+systemctl daemon-reload
+systemctl start kubelet
+systemctl enable kubelet
+
+
+echo "********************************* CRI-O INSTALLATION **************************************"
+
+VERSION=1.22
+
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_7/devel:kubic:libcontainers:stable.repo
+
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:${VERSION}/CentOS_7/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo 
+
+yum install cri-o -y
+systemctl start crio
+systemctl enable crio
